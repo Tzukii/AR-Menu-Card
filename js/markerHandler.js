19adfe687ccefdb9b138a79ef61f59c63e84b7d1
Shivam Maji <70868901+ParadoxFlame@users.noreply.github.com>
2023-01-13 16:23:11 -0800

    Debugging C170

--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,13 +1,12 @@
 AFRAME.registerComponent("markerhandler", {
     init: async function () {
-
         this.el.addEventListener("markerFound", () => {
-            console.log("marker is found")
+            console.log("marker is found");
             this.handleMarkerFound();
         });
 
         this.el.addEventListener("markerLost", () => {
-            console.log("marker is lost")
+            console.log("marker is lost");
             this.handleMarkerLost();
         });
     },
@@ -16,23 +15,25 @@ AFRAME.registerComponent("markerhandler", {
         var buttonDiv = document.getElementById("button-div");
         buttonDiv.style.display = "flex";
 
-        var ratingButton = document.getElementById("rating-button");
         var orderButtton = document.getElementById("order-button");
+        var orderSummaryButtton = document.getElementById("order-summary-button");
 
         // Handling Click Events
-        ratingButton.addEventListener("click", function () {
-            swal({
-                icon: "warning",
-                title: "Rate Dish",
-                text: "Work In Progress"
-            });
-        });
-
         orderButtton.addEventListener("click", () => {
             swal({
                 icon: "https://i.imgur.com/4NZ6uLY.jpg",
-                title: "Thanks For Order!",
-                text: "Your order will be served soon at your table!"
+                title: "Thanks For Order !",
+                text: "  ",
+                timer: 2000,
+                buttons: false
+            });
+        });
+
+        orderSummaryButtton.addEventListener("click", () => {
+            swal({
+                icon: "warning",
+                title: "Order Summary",
+                text: "Work In Progress"
             });
         });
     },
